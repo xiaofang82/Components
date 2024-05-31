9908fea873178c15744101adc5ff5a342d7a537b
XiaoFang <yangxiaofang82@gmail.com>
2024-05-31 12:33:03 -0500

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2023 My Website. All rights reserved.</p>
+      <p>&copy; 2024 My Website. All rights reserved.</p>
     </footer>
   );
 };
